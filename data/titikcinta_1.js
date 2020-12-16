01cc6f1adbd8e26f536aa464ecd1a1881c267e82
brigitta9 <75415492+brigitta9@users.noreply.github.com>
2020-12-16 08:40:14 +0700

    Add files via upload

---
+++ data/titikcinta_1.js
@@ -0,0 +1,8 @@
+var json_titikcinta_1 = {
+"type": "FeatureCollection",
+"name": "titikcinta_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.832736950656042, -6.176100869801671 ] } }
+]
+}
